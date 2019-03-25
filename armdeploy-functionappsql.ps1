ac8269ebb8d01fff6a73af29d6787c5bc4ed0895
Roy Kim (MVP) <roy@roykim.ca>
2019-03-24 21:36:22 -0400

    update

--- armdeploy-functionappsql.ps1
+++ armdeploy-functionappsql.ps1
@@ -24,32 +24,5 @@ New-AzureRmResourceGroup -Name $resourceGroupName  -Location "Canada Central" -F
 $resourceGroupDeployment = New-AzureRmResourceGroupDeployment -Name $resourceGroupName'Deployment' -ResourceGroupName $resourceGroupName `
 -TemplateFile .\\arm-functionapp-sql-main.json -TemplateParameterFile .\armdeploy-functionappsql.paramaters.json -DeploymentDebugLogLevel All -Mode Complete -Force
 
-$resourceGroupDeployment
-$out = $resourceGroupDeployment.Outputs["resourceGroupName"].Value
-$SqlPassword = $resourceGroupDeployment.Outputs["sqlAdministratorLoginPassword"].Value
-
-Get-AzureRmResourceGroupDeploymentOperation -DeploymentName $resourceGroupName'Deployment' -ResourceGroupName $resourceGroupName
-
-Remove-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -Name $resourceGroupName'Deployment' 
-
-# DEBUGGING TROUBLESHOOTING
-$operations = Get-AzureRmResourceGroupDeploymentOperation –DeploymentName $resourceGroupName'Deployment' –ResourceGroupName $resourceGroupName `
-    | Where-Object ProvisioningState -eq Failed
-
-foreach($operation in $operations)
-{
-    Write-Host $operation.id
-    Write-Host "Request:"
-    $operation.Properties.Request | ConvertTo-Json -Depth 10
-    Write-Host "Response:"
-    $operation.Properties.Response | ConvertTo-Json -Depth 10
-}
-
-#### TEST UI Definition
-.\SideLoad-CreateUIDefinition.ps1 -createUIDefFile .\createUiDefinition.json `
-  -StorageResourceGroupLocation canadacentral `
-  -storageContainerName arm-templates -ArtifactsStagingDirectory .\
-
-Get-AzureRmStorageAccount -ResourceGroupName Enterprise -Name rkimstorage
 
 
