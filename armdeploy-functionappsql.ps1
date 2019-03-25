81247d611a1749f48e9ab98e8742a6c7a37ab0b5
Roy Kim (MVP) <rsx79@hotmail.com>
2019-03-24 23:06:14 -0400

    secretsObject

--- armdeploy-functionappsql.ps1
+++ armdeploy-functionappsql.ps1
@@ -16,7 +16,8 @@ $resourceGroupName = "rkfunctionapp-sql"
 New-AzureRmResourceGroup -Name $resourceGroupName  -Location "Canada Central" -Force 
 
 # Just validates the json file.
-Test-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile .\arm-functionapp-sql-main.json -TemplateParameterFile .\armdeploy-functionappsql.paramaters.json
+# from github
+Test-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateUri 'https://raw.githubusercontent.com/RoyKimYYZ/azuredeploy-functionapp-sql-keyvault/master/azuredeploy-functionapp-sql-main.json?token=AB3kogVprN3kK8DUgUVVwWS8p88JqoA3ks5cmDM9wA%3D%3D' -TemplateParameterUri 'https://raw.githubusercontent.com/RoyKimYYZ/azuredeploy-functionapp-sql-keyvault/master/azuredeploy-functionappsql.paramaters.json?token=AB3kogv4Y26g_wIxstwp-dxMsrvcCW5Eks5cmDO7wA%3D%3D'
 
 Remove-AzureRmResourceGroup $resourceGroupName -Force
 New-AzureRmResourceGroup -Name $resourceGroupName  -Location "Canada Central" -Force 
