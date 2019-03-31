e74c2bcf9a18c7bebb4adc5e59d7c16b9bc12e13
Roy Kim <rsx79@hotmail.com>
2019-03-30 23:10:23 -0400

    AzureRm to Az

--- armdeploy-functionappsql.ps1
+++ armdeploy-functionappsql.ps1
@@ -1,30 +1,30 @@
-Enable-AzureRmAlias
+Enable-AzAlias
 Import-Module Az
 
 Connect-AzAccount
-Get-AzureRmContext
+Get-AzContext
 
-Select-AzureRmSubscription -SubscriptionName "Visual Studio Enterprise"
+Select-AzSubscription -SubscriptionName "Visual Studio Enterprise"
 
 Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force -Scope Process
 
 #### Api-version
-((Get-AzureRmResourceProvider -ProviderNamespace Microsoft.Web).ResourceTypes | Where-Object ResourceTypeName -eq sites).ApiVersions
-(Get-AzureRmResourceProvider -ProviderNamespace Microsoft.Web).ResourceTypes | Out-GridView
-$Provider=Get-AzureRmResourceProvider|Out-GridView -PassThru
+((Get-AzResourceProvider -ProviderNamespace Microsoft.Web).ResourceTypes | Where-Object ResourceTypeName -eq sites).ApiVersions
+(Get-AzResourceProvider -ProviderNamespace Microsoft.Web).ResourceTypes | Out-GridView
+$Provider=Get-AzResourceProvider|Out-GridView -PassThru
 $Provider.ResourceTypes
 #####
 $resourceGroupName = "rkfunctionapp-sql"
-New-AzureRmResourceGroup -Name $resourceGroupName  -Location "Canada Central" -Force 
+New-AzResourceGroup -Name $resourceGroupName  -Location "Canada Central" -Force 
 
 # Just validates the json file.
 # from github
-Test-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateUri 'https://raw.githubusercontent.com/RoyKimYYZ/azuredeploy-functionapp-sql-keyvault/master/azuredeploy-functionapp-sql-main.json?token=AB3kogVprN3kK8DUgUVVwWS8p88JqoA3ks5cmDM9wA%3D%3D' -TemplateParameterUri 'https://raw.githubusercontent.com/RoyKimYYZ/azuredeploy-functionapp-sql-keyvault/master/azuredeploy-functionappsql.paramaters.json?token=AB3kogv4Y26g_wIxstwp-dxMsrvcCW5Eks5cmDO7wA%3D%3D'
+Test-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateUri 'https://raw.githubusercontent.com/RoyKimYYZ/azuredeploy-functionapp-sql-keyvault/master/azuredeploy-functionapp-sql-main.json?token=AB3kogVprN3kK8DUgUVVwWS8p88JqoA3ks5cmDM9wA%3D%3D' -TemplateParameterUri 'https://raw.githubusercontent.com/RoyKimYYZ/azuredeploy-functionapp-sql-keyvault/master/azuredeploy-functionappsql.paramaters.json?token=AB3kogv4Y26g_wIxstwp-dxMsrvcCW5Eks5cmDO7wA%3D%3D'
 
-Remove-AzureRmResourceGroup $resourceGroupName -Force
-New-AzureRmResourceGroup -Name $resourceGroupName  -Location "Canada Central" -Force 
+Remove-AzResourceGroup $resourceGroupName -Force
+New-AzResourceGroup -Name $resourceGroupName  -Location "Canada Central" -Force 
 
-$resourceGroupDeployment = New-AzureRmResourceGroupDeployment -Name $resourceGroupName'Deployment' -ResourceGroupName $resourceGroupName `
+$resourceGroupDeployment = New-AzResourceGroupDeployment -Name $resourceGroupName'Deployment' -ResourceGroupName $resourceGroupName `
 -TemplateFile .\\arm-functionapp-sql-main.json -TemplateParameterFile .\armdeploy-functionappsql.paramaters.json -DeploymentDebugLogLevel All -Mode Complete -Force
 
 $resourceGroupDeployment = New-AzResourceGroupDeployment -Name $resourceGroupName'Deployment' -ResourceGroupName $resourceGroupName `
